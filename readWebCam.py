3686e6ae8d58a488c39f3d937b33f4157f9be9c3
l-gahan <lucagahan@gmail.com>
2024-01-19 12:22:29 +0000

    Webcam

---
+++ readWebCam.py
@@ -0,0 +1,38 @@
+# Tutorial https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/?ref=lbp
+
+# import the opencv library 
+import cv2 
+
+def readVideoFromCamera():
+    # define a video capture object 
+    video = cv2.VideoCapture(0) 
+    # if not video.isOpened():
+    #     print("Error: Could not open webcam.")
+    #     exit()
+    while(True): 
+        frame = captureFrame(video)
+        displayFrame(frame)
+        if userExitRequest():
+            break
+    stopReading(video)
+
+def displayFrame(frame):
+    cv2.imshow('frame', frame) 
+        
+def captureFrame(video):
+    frameReturnedSuccessfully, frame = video.read() 
+    return frame
+
+def stopReading(video):
+    '''
+    After the loop release the cap object and destroy all the windows 
+    '''
+    video.release() 
+    cv2.destroyAllWindows() 
+
+def userExitRequest():
+    '''Return True if user presses q'''
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        return True
+    
+readVideoFromCamera()
