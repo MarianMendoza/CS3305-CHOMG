f06f23b69a7394b5b77f6f851c73eec2edc08215
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-20 15:28:58 +0000

    updated sample.py to include noise reduction

--- sample.py
+++ sample.py
@@ -1,15 +1,18 @@
 from greyscale import *
 from imagereader import *
 from backgroundseperator import *
+from noisereduction import *
 
 def run():
     video = readVideoFromCamera()
     separator = createBackgroundSubtractor()
-
+    kernel = createNoiseReductionKernel()
     while True:
         frame = captureFrame(video)
         greyScaleFrame = getFrameInGreyScale(frame)
         foregroundOfFrame = getForegroundOfFrame(greyScaleFrame, separator)
+        foregroundOfFrame = erodeFrameUsingKernel(foregroundOfFrame, kernel)
+        foregroundOfFrame = dilateFrameUsingKernel(foregroundOfFrame, kernel)
         displayFrame(foregroundOfFrame)
         if userExitRequest():
             stopReading()
