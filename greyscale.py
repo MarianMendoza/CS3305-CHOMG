05dd283a556df01c6f3fa7707abfd504e0dca400
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-20 14:06:25 +0000

    Added function to greyscale.py
    Added function which can convert frames to greyscale

--- greyscale.py
+++ greyscale.py
@@ -1,1 +1,7 @@
-
+# Based on tutorial - https://hackthedeveloper.com/motion-detection-opencv-python/
+import cv2
+def getFrameInGreyScale(frame):
+    '''
+    Convert the frame to grayscale
+    '''
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
