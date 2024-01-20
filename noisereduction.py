52f3d944940afaa21c130a192f53d2550c54fea5
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-20 15:31:22 +0000

    adjusted kernel size in noisereduction.py to make noise reduction more refined

--- noisereduction.py
+++ noisereduction.py
@@ -4,7 +4,7 @@ def createNoiseReductionKernel():
     '''
     Creates a kernel in the shape of an ellipse of size 10 up and across to help remove noise in the frame
     '''
-    return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+    return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
 def erodeFrameUsingKernel(frame, kernel):
     return cv2.erode(frame,kernel)
