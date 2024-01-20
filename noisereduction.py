988e32f84337a7f18723c0111052f75ad887f25e
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-20 15:26:23 +0000

    Created functions in noisereduction to remove noise from frames

---
+++ noisereduction.py
@@ -0,0 +1,13 @@
+import cv2
+
+def createNoiseReductionKernel():
+    '''
+    Creates a kernel in the shape of an ellipse of size 10 up and across to help remove noise in the frame
+    '''
+    return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+
+def erodeFrameUsingKernel(frame, kernel):
+    return cv2.erode(frame,kernel)
+
+def dilateFrameUsingKernel(frame, kernel):
+    return cv2.dilate(frame, kernel)
