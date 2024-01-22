df933f62be477a4265a53c89e87eeaedd4e693de
MarianMendoza <115280525+MarianMendoza@users.noreply.github.com>
2024-01-22 20:54:04 +0000

    Created: activity_login.xml shape.xml LoginActivity.java
    Edited:
    AndroidManifest.xml
    
    Created a login in login activity with .xml in themes. Configured android manifest. Might need to change permissions. Add config, and configure it so that instead of main, it runs login activity.Base template of login. Will edit buttons and login.
    
    Added font.

---
+++ app/src/main/java/com/example/chomg/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.chomg;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
