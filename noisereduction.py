a619be58f26733d1ffeebcc4427d0147efbaaf54
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 23:39:50 +0000

    Updated function/variable names to snake case in noisereduction.py in line with PEP 8

--- noisereduction.py
+++ noisereduction.py
@@ -1,13 +1,13 @@
 import cv2
 
-def createNoiseReductionKernel():
+def create_noise_reduction_kernel():
     '''
     Creates a kernel in the shape of an ellipse of size 3 up and across to help remove noise in the frame
     '''
     return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-def erodeFrameUsingKernel(frame, kernel):
-    return cv2.erode(frame,kernel)
+def erode_frame_using_kernel(frame, kernel):
+    return cv2.erode(frame, kernel)
 
-def dilateFrameUsingKernel(frame, kernel):
+def dilate_frame_using_kernel(frame, kernel):
     return cv2.dilate(frame, kernel)
