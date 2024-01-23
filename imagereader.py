59ec878875e42b22cddfde5f3130ab68ada61cfc
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 23:39:34 +0000

    Updated function/variable names to snake case in imagereader.py in line with PEP 8

--- imagereader.py
+++ imagereader.py
@@ -11,34 +11,34 @@
 # import the opencv library 
 import cv2 
 
-def readVideoFromCamera():
+def read_video_from_camera():
     # define a video capture object 
-    video= cv2.VideoCapture(0) 
+    video = cv2.VideoCapture(0) 
     return video
     
-def captureFrame(video):
-    frameReturnedSuccessfully, frame = video.read() 
+def capture_frame(video):
+    frame_returned_successfully, frame = video.read() 
     return frame
 
-def displayVideo(video):
+def display_video(video):
     while(True): 
-        frame = captureFrame(video)
-        displayFrame(frame)
-        if userExitRequest():
+        frame = capture_frame(video)
+        display_frame(frame)
+        if user_exit_request():
             break
-    stopReading(video)
+    stop_reading(video)
 
-def displayFrame(frame, nameOfFrame=None):
-    if nameOfFrame == None:
-        nameOfFrame =""
-    cv2.imshow(nameOfFrame, frame) 
+def display_frame(frame, name_of_frame = None):
+    if name_of_frame == None:
+        name_of_frame = ""
+    cv2.imshow(name_of_frame, frame) 
     
-def userExitRequest():
+def user_exit_request():
     '''Return True if user presses q'''
     if cv2.waitKey(1)  == ord('q'): 
         return True
     
-def stopReading(video):
+def stop_reading(video):
     '''
     After the loop release the cap object and destroy all the windows 
     '''
