724ea23893e34bd2d56958596a208855519135bb
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 23:38:50 +0000

    Updated function/variable names to snake case in cropframe.py in line with PEP 8

--- cropframe.py
+++ cropframe.py
@@ -1,9 +1,9 @@
 import cv2
 # Reference - https://stackoverflow.com/questions/60869306/how-to-simple-crop-the-bounding-box-in-python-opencv
-def cropFrameToBoundingBox(frame, boundingBox):
-    x = boundingBox.x1
-    y = boundingBox.y1
-    height= boundingBox.y2
-    width = boundingBox.x2
-    croppedFrame = frame[y: y + height, x: x + width]
-    return croppedFrame
+def crop_frame_to_bounding_box(frame, bounding_box):
+    x = bounding_box.x1
+    y = bounding_box.y1
+    height= bounding_box.y2
+    width = bounding_box.x2
+    cropped_frame = frame[y: y + height, x: x + width]
+    return cropped_frame
