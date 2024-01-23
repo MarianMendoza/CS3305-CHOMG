c79142786a841298cdfcdf37320e466e8218c5bc
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 19:23:48 +0000

    renamed function in display.py to be more accurate with its use

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 # https://python-win32.python.narkive.com/3e75rNuo/turning-monitors-off-from-python
 import  win32con
 import win32gui
-def turnOffDisplay():
+def turnOffWindowsDisplay():
     SC_MONITORPOWER = 0xF170
     win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
