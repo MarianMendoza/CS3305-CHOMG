09764d4b7296c1c6db27386ead1d24c7927cf45e
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 09:24:50 +0000

    Created display.py which contains turnOffDisplay function which turns off the users monitors when called on windows

---
+++ display.py
@@ -0,0 +1,6 @@
+# https://python-win32.python.narkive.com/3e75rNuo/turning-monitors-off-from-python
+import  win32con
+import win32gui
+def turnOffDisplay():
+    SC_MONITORPOWER = 0xF170
+    win32gui.SendMessage(win32con.HWND_BROADCAST, win32con.WM_SYSCOMMAND, SC_MONITORPOWER, 2)
