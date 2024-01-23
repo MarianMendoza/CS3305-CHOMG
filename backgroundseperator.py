168d23cef8879214917280ee8a99af5ea717ae4b
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-23 23:36:55 +0000

    Updated function/variable names to snake case in backgroundseperator.py in line with PEP 8

--- backgroundseperator.py
+++ backgroundseperator.py
@@ -1,14 +1,14 @@
 import cv2
 
-def createBackgroundSubtractor():
+def create_background_subtractor_object():
     '''
     Creates object which distinguishes foreground from background of images using mog algorithm\n
     Works more efficiently with grey-scale frames
     '''
     return cv2.createBackgroundSubtractorMOG2()
 
-def getForegroundOfFrame(greyscaleFrame, subtractor):
-    return subtractor.apply(greyscaleFrame)
+def get_foreground_of_frame_using_subtractor_object(frame, subtractor):
+    return subtractor.apply(frame)
 
-def recolourForegroundUsingOriginalFrame(foregroundOfFrame, frame): #https://www.youtube.com/watch?v=YSLVAxgclCo
-    return cv2.bitwise_and(frame, frame, mask=foregroundOfFrame)
+def recolour_foreground_using_original_frame(foreground_of_frame, frame): #https://www.youtube.com/watch?v=YSLVAxgclCo
+    return cv2.bitwise_and(frame, frame, mask=foreground_of_frame)
