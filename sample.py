2a9710e9a5849f2b93e4588cced013a3956f8288
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-24 12:50:40 +0000

    Separated code into functions temporarily in sample.py for readability

--- sample.py
+++ sample.py
@@ -5,20 +5,37 @@ import noise_reduction
 import bounding_box
 import entity_recognition 
 import crop_frame
-
+import display
 def run():
     video = image_reader.read_video_from_camera()
     separator = background_seperator.create_background_subtractor_object()
     kernel = noise_reduction.create_noise_reduction_kernel()
+    # display.turn_off_monitor_display()
     while True:
+        frame, foreground_of_frame = handle_video_frame(video, separator, kernel)
+
+        contours = bounding_box.get_contours(foreground_of_frame)
+        if contours:
+            frame = handle_motion_detection_in_frame_using_contours(contours, frame)
+
+        image_reader.display_frame(frame, "bg")
+        image_reader.display_frame(foreground_of_frame, "fg")
+
+        if image_reader.user_exit_request():
+            image_reader.stop_reading(video)
+            break
+
+
+
+def handle_video_frame(video, separator, kernel):
         frame = image_reader.capture_frame(video)
         grey_scale_frame = grey_scale.get_frame_in_grey_scale(frame)
         foreground_of_frame = background_seperator.get_foreground_of_frame_using_subtractor_object(grey_scale_frame, separator)
         foreground_of_frame = noise_reduction.erode_frame_using_kernel(foreground_of_frame, kernel)
         foreground_of_frame = noise_reduction.dilate_frame_using_kernel(foreground_of_frame, kernel)
+        return frame, foreground_of_frame
 
-        contours = bounding_box.get_contours(foreground_of_frame)
-        if contours:
+def handle_motion_detection_in_frame_using_contours(contours, frame):
             max_contour = bounding_box.get_max_contour(contours)
             approximate_polygonal_curve = bounding_box.get_approximate_curve_from_contour(max_contour)
 
@@ -30,12 +47,7 @@ def run():
                 print(True)
 
             bounding_box.draw_bounding_box_on_frame(frame, bounding_box_coordinates)
-
-        image_reader.display_frame(frame, "bg")
-        image_reader.display_frame(foreground_of_frame, "fg")
-        if image_reader.user_exit_request():
-            image_reader.stop_reading(video)
-            break
+            return frame
 
 if __name__ == "__main__":
     run()
