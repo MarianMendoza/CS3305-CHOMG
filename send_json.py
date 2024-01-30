11bb3bc12ad54a98c847f5881569f42d32105899
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-30 15:38:11 +0000

    Added variable hint to send_json

--- send_json.py
+++ send_json.py
@@ -3,7 +3,7 @@ import requests
 
 class JsonSender(object):
 
-    def __init__(self, destination_ip_address) -> None:
+    def __init__(self, destination_ip_address: str) -> None:
         self.destination_ip_address = destination_ip_address
 
     def send_is_motion_detected(self, motion_detected: bool):
