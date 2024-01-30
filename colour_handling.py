d5fd69e2c7b5dcefa97ae01b3bb678f50bd2dc8a
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-30 17:45:37 +0000

    Refactored related colour conversion functions into colour_handling.py file

--- colour_handling.py
+++ colour_handling.py
@@ -5,3 +5,9 @@ def get_frame_in_grey_scale(frame):
     Convert the frame to grayscale
     '''
     return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+def get_frame_in_rgb(frame):
+    '''
+    Convert the frame to RGB
+    '''
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
--- rgb_frame.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def get_frame_in_rgb(frame):
-    '''
-    Convert the frame to RGB
-    '''
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
