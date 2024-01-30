3e84a18761d9235dd2465343f2cd5a73befd612a
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-30 22:19:25 +0000

    Added function in JsonSender to receive json tokens

--- send_json.py
+++ send_json.py
@@ -6,5 +6,10 @@ class JsonSender(object):
     def __init__(self, destination_ip_address: str) -> None:
         self.destination_ip_address = destination_ip_address
 
-    def send_is_motion_detected(self, motion_detected: bool):
-        requests.post(self.destination_ip_address, (motion_detected))
+    def send_is_motion_detected(self, motion_detected: bool, person_detected: bool, connection_lost: bool, program_terminated: bool):
+        requests.post(self.destination_ip_address, (motion_detected, person_detected , connection_lost , program_terminated ))
+
+    # Reference
+    def get_user_information(self):
+        response_data = requests.get(self.destination_ip_address)
+        return ...
