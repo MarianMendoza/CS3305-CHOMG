4ccf67c6b8b15e77a03faf7d71088c9628faf6da
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-01-30 15:17:11 +0000

    Added class to send-json to handle sending json tokens

---
+++ send_json.py
@@ -0,0 +1,10 @@
+# Reference - https://reqbin.com/code/python/m2g4va4a/python-requests-post-json-example
+import requests
+
+class JsonSender(object):
+
+    def __init__(self, destination_ip_address) -> None:
+        self.destination_ip_address = destination_ip_address
+
+    def send_is_motion_detected(self, motion_detected: bool):
+        requests.post(self.destination_ip_address, (motion_detected))
