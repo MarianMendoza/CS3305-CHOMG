756d50deb5be70497f1be95bed4340b4458bc1ae
MarianMendoza <115280525+MarianMendoza@users.noreply.github.com>
2024-02-05 17:31:59 +0000

    Made bottom navigation bar and started making home page, changing ui. Added 2 dependencies.

--- app/src/main/java/com/example/chomg/HomeActivity.java
+++ app/src/main/java/com/example/chomg/HomeActivity.java
@@ -1,343 +1,42 @@
 package com.example.chomg;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Date;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-
-
-//THIS IS THE CAMERA
 public class HomeActivity extends AppCompatActivity {
-
-
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 101;
-
-
-    private CameraManager cameraManager;
-    private String cameraId;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSession;
-    private CaptureRequest.Builder captureRequestBuilder;
-
-    private SurfaceView surfaceView;
-
-    private HandlerThread backgroundThread;
-    private Handler backgroundHandler;
-
-    private TextView timeTextView;
-    private TextView locationTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        surfaceView = findViewById(R.id.previewView);
-        timeTextView = findViewById(R.id.timeView);
-        locationTextView = findViewById(R.id.LocationView);  // Added for location
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new ExampleFragment1())
+                .commit();
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{Manifest.permission.CAMERA},
-                    CAMERA_PERMISSION_REQUEST_CODE
-            );
-        } else {
-            initializeCamera();
-        }
-        // Check location permission and initialize location if granted
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE
-            );
-        } else {
-            initializeLocation();
-        }
-    }
-
-    private void initializeLocation() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // Define a location listener
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
-
-                // Reverse geocode the location to get the area
-                Geocoder geocoder = new Geocoder(HomeActivity.this, Locale.getDefault());
-                try {
-                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
-
-                    if (!addresses.isEmpty()) {
-                        String area = addresses.get(0).getLocality();
-                        // Set the formatted area to the TextView
-                        locationTextView.setText("Location: " + area);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (item.getItemId() == R.id.menu_itemHome) {
+                selectedFragment = new ExampleFragment1();
+            } else if (item.getItemId() == R.id.menu_itemAccount) {
+                // Replace with the fragment for the "Account" menu item
+            } else if (item.getItemId() == R.id.menu_itemSettings) {
+                // Replace with the fragment for the "Settings" menu item
             }
 
-            // Other location listener methods...
-
-        };
-
-        // Request location updates
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    0,
-                    0,
-                    locationListener
-            );
-        }
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        String currentTime = dateFormat.format(new Date());
-
-        // Set the formatted time to the TextView
-        timeTextView.setText("Time: " + currentTime);
-
-    }
-
-
-    private void initializeCamera() {
-        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-        try {
-            cameraId = cameraManager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-        surfaceView.getHolder().addCallback(surfaceCallback);
-        try {
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
-            StreamConfigurationMap streamConfigMap = characteristics.get(
-                    CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-
-            // Choose a suitable preview size with the desired aspect ratio
-            Size[] sizes = streamConfigMap.getOutputSizes(SurfaceHolder.class);
-            Size previewSize = chooseOptimalSize(sizes, 6, 6); // Adjust aspect ratio as needed
-
-            // Set the chosen preview size
-            surfaceView.getHolder().setFixedSize(previewSize.getWidth(), previewSize.getHeight());
-
-            // ... rest of your camera initialization code
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Size chooseOptimalSize(Size[] choices, int targetWidth, int targetHeight) {
-        List<Size> bigEnough = new ArrayList<>();
-        for (Size option : choices) {
-            if (option.getHeight() == option.getWidth() * targetHeight / targetWidth &&
-                    option.getWidth() >= targetWidth && option.getHeight() >= targetHeight) {
-                bigEnough.add(option);
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainer, selectedFragment)
+                        .commit();
+                return true;
+            } else {
+                return false;
             }
-        }
-
-        if (bigEnough.size() > 0) {
-            return Collections.min(bigEnough, new CompareSizesByArea());
-        } else {
-            return choices[0];
-        }
-    }
-
-    private static class CompareSizesByArea implements Comparator<Size> {
-        @Override
-        public int compare(Size lhs, Size rhs) {
-            // We cast here to ensure the multiplications won't overflow
-            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
-                    (long) rhs.getWidth() * rhs.getHeight());
-        }
-    }
-
-    private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            openCamera();
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            holder.setFixedSize(1270, 720);
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            closeCamera();
-        }
-    };
-
-    private void openCamera() {
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_GRANTED) {
-                cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            cameraDevice = camera;
-            createCameraPreviewSession();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            closeCamera();
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            closeCamera();
-        }
-    };
-
-    private void createCameraPreviewSession() {
-        try {
-            Surface surface = surfaceView.getHolder().getSurface();
-
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            captureRequestBuilder.addTarget(surface);
-
-            cameraDevice.createCaptureSession(Collections.singletonList(surface),
-                    new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(@NonNull CameraCaptureSession session) {
-                            if (cameraDevice == null) {
-                                return;
-                            }
-
-                            cameraCaptureSession = session;
-                            updatePreview();
-                        }
-
-                        @Override
-                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                            Log.e("CameraTestActivity", "Camera capture session configuration failed");
-                        }
-                    }, backgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updatePreview() {
-        if (cameraDevice == null) {
-            return;
-        }
-
-        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
-
-        try {
-            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(),
-                    new CameraCaptureSession.CaptureCallback() {
-                        @Override
-                        public void onCaptureCompleted(@NonNull CameraCaptureSession session,
-                                                       @NonNull CaptureRequest request,
-                                                       @NonNull TotalCaptureResult result) {
-                            // Do something when capture is completed
-                        }
-                    }, backgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void closeCamera() {
-        if (cameraCaptureSession != null) {
-            cameraCaptureSession.close();
-            cameraCaptureSession = null;
-        }
-
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        startBackgroundThread();
-    }
-
-    @Override
-    protected void onPause() {
-        closeCamera();
-        stopBackgroundThread();
-        super.onPause();
-    }
-
-    private void startBackgroundThread() {
-        backgroundThread = new HandlerThread("CameraBackground");
-        backgroundThread.start();
-        backgroundHandler = new Handler(backgroundThread.getLooper());
-    }
-
-    private void stopBackgroundThread() {
-        if (backgroundThread != null) {
-            backgroundThread.quitSafely();
-            try {
-                backgroundThread.join();
-                backgroundThread = null;
-                backgroundHandler = null;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        });
     }
 }
