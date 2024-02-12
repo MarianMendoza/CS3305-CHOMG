33e7bb303bbabb591ce3f7d1346b05ca14816f46
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-02-12 15:16:18 +0000

    Updated recording_sample.py to account for changes in record_on_movement.py

--- server_webcam_processing/recording_sample.py
+++ server_webcam_processing/recording_sample.py
@@ -5,6 +5,7 @@ import bounding_box
 
 def run():
     frame_handler = video_frame_handling.VideoFrameHandler()
+    frame_recorder = record_on_movement.Recorder(frame_handler.get_current_frame())
     try:
         while True:
             contours = frame_handler.get_contours_of_current_frame()
@@ -13,12 +14,13 @@ def run():
             max_contour = bounding_box.get_max_contour(contours, min_area=1000)  # Adjust min_area as needed
             is_movement_detected = max_contour is not None
             
-            if contours and is_movement_detected:
+            if is_movement_detected:
                 frame_handler.handle_motion_detection_in_frame_using_contours(contours)
+                # Record based on significant movement detection
+                frame_recorder.record_frame(frame_handler.get_current_frame())
             
-            # Record based on significant movement detection
-            record_on_movement.record_on_movement(frame_handler.current_frame, is_movement_detected)
-            
+            else:
+                frame_recorder.stop_recording_if_time_elapsed(frame_handler.get_current_frame())
             # Displaying the current frame and optional foreground
             frame_handler.display_current_frame()
             # frame_handler.display_foreground()
