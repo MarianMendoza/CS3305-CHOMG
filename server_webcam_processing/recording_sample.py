daacf6b711c45ecb87c5372f12bdb13c7837a814
l-gahan <93290342+l-gahan@users.noreply.github.com>
2024-02-13 10:52:56 +0000

    Adjustments for code readability

--- server_webcam_processing/recording_sample.py
+++ server_webcam_processing/recording_sample.py
@@ -1,7 +1,6 @@
 import image_reader
 import video_frame_handling
 import record_on_movement
-import bounding_box
 import linked_list_file_saver
 def run():
     frame_handler = video_frame_handling.VideoFrameHandler()
@@ -9,20 +8,14 @@ def run():
     linked_list = linked_list_file_saver.LinkedList()
     try:
         while True:
-            contours = frame_handler.get_contours_of_current_frame()
-            
-            # Determine if significant movement is detected based on max_contour
-            max_contour = bounding_box.get_max_contour(contours, min_area=1000)  # Adjust min_area as needed
-            is_movement_detected = max_contour is not None
-            
-            if is_movement_detected:
+            if frame_handler.is_movement_detected():
                 if not frame_recorder.is_recording():
                     if not linked_list.is_empty():
                         for frame in linked_list.get_list_of_frames_in_linked_list():
                             frame_recorder.record_frame(frame)
                         linked_list.clear_linked_list()
                 else:
-                    frame_handler.handle_motion_detection_in_frame_using_contours(contours)
+                    frame_handler.handle_motion_detection_in_frame_using_contours()
                     # Record based on significant movement detection
                     frame_recorder.record_frame(frame_handler.get_current_frame())
             
@@ -30,7 +23,7 @@ def run():
                 frame_recorder.stop_recording_if_time_elapsed(frame_handler.get_current_frame())
             # Displaying the current frame and optional foreground
             frame_handler.display_current_frame()
-            # frame_handler.display_foreground()
+            frame_handler.display_foreground()
             
             if not frame_recorder.is_recording():
                 # Save frame in linked list
--- server_webcam_processing/video_frame_handling.py
+++ server_webcam_processing/video_frame_handling.py
@@ -22,6 +22,9 @@ class VideoFrameHandler(object):
         self.__set_adjusted_foreground_of_current_frame()
         self.__current_foreground_of_frame = self.get_adjusted_foreground_of_current_frame()
         self.__xml_data_for_human_detection = cv2.CascadeClassifier(os.path.join(sys._MEIPASS, 'haarcascade_fullbody.xml')) if getattr(sys, 'frozen', False) else cv2.CascadeClassifier('haarcascade_fullbody.xml')
+        self.__set_contours_of_current_frame()
+        self.__set_max_contour_of_current_frame()
+        self.__set_movement_detected()
 
     def get_adjusted_foreground_of_current_frame(self):
         return self.__current_foreground_of_frame
@@ -39,11 +42,10 @@ class VideoFrameHandler(object):
         return self.current_frame
     
     # TODO: Clean this function
-    def handle_motion_detection_in_frame_using_contours(self, contours):
-        max_contour = bounding_box.get_max_contour(contours)
-        if max_contour is None:
+    def handle_motion_detection_in_frame_using_contours(self):
+        if self.max_contour is None:
             return
-        approximate_polygonal_curve = bounding_box.get_approximate_curve_from_contour(max_contour)
+        approximate_polygonal_curve = bounding_box.get_approximate_curve_from_contour(self.max_contour)
         bounding_box_coordinates = bounding_box.get_bounding_box_from_curve(approximate_polygonal_curve)
         cropped_frame = crop_frame.crop_frame_to_bounding_box(self.current_frame, bounding_box_coordinates)
         if self.__is_person_detected_in_cropped_frame(cropped_frame):
@@ -51,8 +53,24 @@ class VideoFrameHandler(object):
         bounding_box.draw_bounding_box_on_frame(self.current_frame, bounding_box_coordinates)
     
     def get_contours_of_current_frame(self):
-        return bounding_box.get_contours(self.__current_foreground_of_frame)
+        return self.contours_of_current_frame
 
+    def __set_contours_of_current_frame(self):
+        self.contours_of_current_frame = bounding_box.get_contours(self.__current_foreground_of_frame)
+        
+
+    def __set_max_contour_of_current_frame(self):
+        self.max_contour = bounding_box.get_max_contour(self.contours_of_current_frame, min_area=1000) 
+
+    def __set_movement_detected(self):
+        if self.max_contour is not None:
+            self.movement_detected = True
+        else:
+            self.movement_detected = False
+
+    def get_max_contour_of_current_frame(self):
+        return self.max_contour
+        
     def __is_person_detected_in_cropped_frame(self, cropped_frame):
         '''
         Takes cropped frame as input
@@ -78,3 +96,9 @@ class VideoFrameHandler(object):
     def set_next_frame_as_current(self):
         self.current_frame = image_reader.capture_frame(self.__video)
         self.__set_adjusted_foreground_of_current_frame()
+        self.__set_contours_of_current_frame()
+        self.__set_max_contour_of_current_frame()
+        self.__set_movement_detected()
+        
+    def is_movement_detected(self):
+        return self.movement_detected
