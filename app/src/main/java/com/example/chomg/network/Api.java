41648b43f0677f75bd1c4a28c87cfc048efe0c50
liam140303 <92456063+liam140303@users.noreply.github.com>
2024-02-16 12:20:56 +0000

    Added delete account functionality

--- app/src/main/java/com/example/chomg/network/Api.java
+++ app/src/main/java/com/example/chomg/network/Api.java
@@ -7,6 +7,7 @@ import com.example.chomg.data.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
@@ -23,6 +24,9 @@ public interface Api {
 
     @POST("/forgot-password")
     Call<ResponseBody> forgotPassword(@Body EmailWrapper emailWrapper);
+
+    @DELETE("/delete-account")
+    Call<Void> deleteAccount(@Header("Authorization") String authToken);
 
     @GET("/user-details")
     Call<User> getUserDetails(@Header("Authorization") String authToken);
