d56fe2e2e94597a6b98c2b5ea53e8eeaf025ff65
MarianMendoza <115280525+MarianMendoza@users.noreply.github.com>
2024-02-24 18:43:53 +0000

    Activity log changed, download button added, empty button listen.

--- app/src/main/java/com/example/chomg/userinterface/FragmentHome.java
+++ app/src/main/java/com/example/chomg/userinterface/FragmentHome.java
@@ -4,9 +4,13 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import okhttp3.ResponseBody;
@@ -48,7 +52,63 @@ public class FragmentHome extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         playerView = view.findViewById(R.id.playerView);
         initializePlayer();
+
+        LinearLayout chatContainer = view.findViewById(R.id.chatContainer);
+
+        String[] activityNames = {"Activity 1", "Activity 2", "Activity 3", "Activity 4", "Activity 5","Activity 6","Activity 7","Activity 8"};
+
+        for (String activityName : activityNames) {
+            LinearLayout horizontalLayout = new LinearLayout(requireContext());
+            horizontalLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView textView = new TextView(requireContext());
+            LinearLayout.LayoutParams textLayoutParams = new LinearLayout.LayoutParams(
+                    0,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1
+            );
+            textLayoutParams.setMargins(0, 8, 0, 0);
+            textView.setLayoutParams(textLayoutParams);
+            textView.setBackgroundResource(R.drawable.shape1);
+            textView.setPadding(8, 8, 8, 8);
+            textView.setWidth(2000);
+            textView.setHeight(150);
+            textView.setText(activityName);
+            textView.setGravity(Gravity.START);
+            textView.setTextSize(16);
+
+            // Create Button
+            Button button = new Button(requireContext());
+            LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            buttonLayoutParams.gravity = Gravity.END | Gravity.CENTER_VERTICAL;
+            button.setLayoutParams(buttonLayoutParams);
+            button.setCompoundDrawablesWithIntrinsicBounds(R.drawable.download_24px, 0, 0, 0); // Set icon drawable
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Handle button click here
+                    handleButtonClick(activityName);
+                }
+            });
+            horizontalLayout.addView(textView);
+            horizontalLayout.addView(button);
+
+            chatContainer.addView(horizontalLayout);
+        }
+
         return view;
+    }
+
+    private void handleButtonClick(String activityName) {
+        System.out.println("Button Click");
     }
 
     @OptIn(markerClass = UnstableApi.class) private void initializePlayer() {
